61b54d5c5bc48fb3707d5637121996e4cbd3faa3
MinaMagdy2 <140406888+MinaMagdy2@users.noreply.github.com>
2024-12-01 00:25:26 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      body: SafeArea(
-        child: WebView(
-          initialUrl: "https://minamagdy2.github.io/working/",
-          javascriptMode: JavascriptMode.unrestricted,
-        ),
-      ),
-    ),
+    home: MyWebView(),
   ));
 }
 
+class MyWebView extends StatelessWidget {
+  const MyWebView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // إضافة معلمة عشوائية إلى الرابط لمنع التخزين المؤقت
+    String url = "https://minamagdy2.github.io/working/?v=${Random().nextInt(10000)}";
+
+    return Scaffold(
+      body: SafeArea(
+        child: WebView(
+          initialUrl: url,
+          javascriptMode: JavascriptMode.unrestricted,
+        ),
+      ),
+    );
+  }
+}
